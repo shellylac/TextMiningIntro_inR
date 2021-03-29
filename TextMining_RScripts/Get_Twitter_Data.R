51b820ffaeaa67b21dfef3cf4bf1ea390684c129
Shelly Lachish <shellstar@gmail.com>
2021-03-29 12:28:07 +0100

    added directions for other to get their own twitter data

--- TextMining_RScripts/Get_Twitter_Data.R
+++ TextMining_RScripts/Get_Twitter_Data.R
@@ -4,25 +4,28 @@ library(tidyverse)
 library(data.table)
 library(here)
 
+## To get started you’ll need to do the following things:
+##1.  Set up a twitter account if you don’t have one already.
+##2. Using your account, setup an application that you will use to access twitter from R
+   #Follow these directions
+   #https://cran.r-project.org/web/packages/rtweet/vignettes/auth.html
+
+#Then -->:
+
 # whatever name you assigned to your created app
-#appname <- rstudioapi::showPrompt("Please enter your api app name")
-appname <- "ndph_textminingtute"
+appname <- rstudioapi::showPrompt("Please enter your api app name")
 
 ## api key 
-#api_key<-rstudioapi::askForPassword("Please enter your api key")
-key <- "rcifS2T4qzwPd4vNpqDOVRto6"
+api_key<-rstudioapi::askForPassword("Please enter your api key")
 
 ## api secret
-#secret_key<-rstudioapi::askForPassword("Please enter your api secret key")
-secret <- "pAczxrijl26k2RkcYwpHClk8P2O2glkrglsRxffpOIWHX9d5fv"
+secret_key<-rstudioapi::askForPassword("Please enter your api secret key")
 
 ## access token
-#access_token<-rstudioapi::askForPassword("Please enter your access token")
-access_token <- "888992125-9nkoDcz6L3FxQChC67CzQyLDpWmTcKef9ideNW1v"
+access_token<-rstudioapi::askForPassword("Please enter your access token")
 
 ## api access token 
-#access_secret<-rstudioapi::askForPassword("Please enter your access secret token")
-access_secret <- "R5TJ3ZopqZsxHLHojJw4xEV8uzhYXW02V37LmIqOs8fbC"
+access_secret<-rstudioapi::askForPassword("Please enter your access secret token")
 
 #Create a token that authenticates access to tweets.
 #If authentication is successful works, it should render the following message in a browser window:
@@ -36,11 +39,12 @@ twitter_token <- create_token(
   access_secret = access_secret)
 
 #search_tweets only gets tweets from past 6-9 days
+#Run this daily to build up a database over a week
 covid19_tweets_orig <- search_tweets('covid19', n=1000, include_rts=FALSE, lang='en')
 (filename<-paste0(here('Original_Data//'), 'covid19_tweets_', Sys.Date(), '.csv'))
 write_as_csv(covid19_tweets_orig, filename)
 
-
+#This section will combine all the tweet data subsets from different days
 #Combine all the twitter data from different days
 data.files<-list.files(here('Original_Data/'), '.csv', full.names = T)
 twitter_data_full<-bind_rows(lapply(data.files, read_csv))
